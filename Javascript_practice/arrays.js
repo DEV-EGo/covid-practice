e2e1e57f0d0bff03ccdb0ce5885d5b19a421ec01
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-08 19:23:42 -0400

    practice with forloops

--- Javascript_practice/arrays.js
+++ Javascript_practice/arrays.js
@@ -1,4 +1,23 @@
 var MoneyBag = ["cashflow", "infinite", "ecorp"];
+var shoeBrands = ["jordan", "NIKE", "OFFWHITE"];
 
-console.log("FirstStatement");
-console.log(MoneyBag[0]);
+console.log("===========FirstStatement============");
+
+// console.log(MoneyBag[0]);
+
+// console.log(MoneyBag[1]);
+
+// console.log(MoneyBag[2]);
+
+for (var i = 0; i < MoneyBag.length; i++) {
+    console.log(" This is one of my investments" + MoneyBag[i]);
+}
+
+console.log("===========2ndstatement============");
+
+
+for (var i = 0; i < shoeBrands.length; i++) {
+    console.log(" what size " + shoeBrands[i] + " do you need? ");
+}
+
+console.log("===========2ndstatement============");
