5bc8c5515457db2233b9df8d6e3e6dd1c20843ca
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-08 20:19:46 -0400

    practice with forloops

--- Javascript_practice/arrays.js
+++ Javascript_practice/arrays.js
@@ -20,4 +20,29 @@ for (var i = 0; i < shoeBrands.length; i++) {
     console.log(" what size " + shoeBrands[i] + " do you need? ");
 }
 
-console.log("===========2ndstatement============");
+console.log("===========3rdstatement============");
+
+var exotic = [" McClaren", " Lambo", " Ferrari", " Bugatti"];
+
+for (var i = 0; i < exotic.length; i++) {
+    console.log(" You Unlocked" + exotic[i]);
+}
+
+console.log("===========4thstatement============");
+
+var movies = [" Matrix", " Godzilla", " Fast n Furious", " Pokemon"];
+
+for (var i = 0; i < movies.length; i++) {
+    console.log(movies[i] + " was a blockBuster");
+}
+
+console.log("===========5thstatement============");
+
+
+var Avenger = [" Iron Man", " HULK ", " THOR ", " Thanos ", " Black Panther "];
+
+for (var i = 0; i < Avenger.length; i++) {
+    console.log(Avenger[i] + " is a mighty hero");
+}
+
+console.log("===========6thstatement============");
