73e280936719db4862b16677fd406d1e98b3c3a4
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-11 13:59:42 -0400

    harder looops

---
+++ Javascript_practice/19-AnotherLoop/loop.js
@@ -0,0 +1,19 @@
+// ### Instructions
+
+// * Starting from scratch, create a `for` loop that console logs the following lines:
+
+// ```
+// I am 0
+// I am 1
+// I am 2
+// I am 3
+// I am 4
+// ```
+
+// * **NOTE:** Don't use an array!
+function Myself(i) {
+    for (var i = 0; i < 5; i++) {
+        console.log("i am " + i);
+    }
+}
+Myself(i);
