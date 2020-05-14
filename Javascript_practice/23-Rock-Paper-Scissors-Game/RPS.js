c6d2a3a6fbdd9784de1cdc1e439e9a34c58aa1a1
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-14 14:28:29 -0400

    first sucess

--- Javascript_practice/23-Rock-Paper-Scissors-Game/RPS.js
+++ Javascript_practice/23-Rock-Paper-Scissors-Game/RPS.js
@@ -2,6 +2,12 @@ var GamerChoices = ["r", "p", "s"];
 
 document.onkeyup = function (event) {
 
+    var keyGuess = event.key;
 
+    var UserGuess = GamerChoices[Math.floor(Math.random() * GamerChoices.length)];
 
-}
+    alert(" User Guesse: " + keyGuess);
+
+    alert(" computer Guessed " + UserGuess);
+
+};
