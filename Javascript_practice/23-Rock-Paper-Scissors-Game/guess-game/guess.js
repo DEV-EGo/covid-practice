b6d1547e30bea55c03d31e0cb51b2b47908dcf3a
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-14 22:30:41 -0400

    update

---
+++ Javascript_practice/23-Rock-Paper-Scissors-Game/guess-game/guess.js
@@ -0,0 +1,13 @@
+var PCchoice = ["r", "p", "s"];
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var Instructionstext = document.getElementById("Instruction-text");
+var PlayerChoiceText = document.getElementById("PlayerChoice-text");
+var ComputerChoiceText = document.getElementById("ComputerChoice-text");
+var WinsText = document.getElementById("wins-text");
+var LossesText = document.getElementById("losses-text");
+var TiesText = document.getElementById("ties-text");
+
