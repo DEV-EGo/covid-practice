ba8fd32865836b9b2ac039f3737f2914b6a93ca9
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-15 14:07:27 -0400

    file set up

---
+++ Javascript_practice/23-Rock-Paper-Scissors-Game/Students/students.js
@@ -0,0 +1,11 @@
+// create a row of students
+
+// Create an entire classroom
+
+// print out every student in  row 1
+
+// print out every student in row2 that begins with the letter J
+
+// how to acess second students name in row3
+
+// how do we acess the third student in a row2?
