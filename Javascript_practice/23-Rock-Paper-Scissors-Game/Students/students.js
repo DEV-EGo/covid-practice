7b10ebbc5efd22bb23a45a8cccde8a5c522e8581
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-15 14:31:35 -0400

    set up

--- Javascript_practice/23-Rock-Paper-Scissors-Game/Students/students.js
+++ Javascript_practice/23-Rock-Paper-Scissors-Game/Students/students.js
@@ -1,11 +1,25 @@
 // create a row of students
+var StudentRow1 = ["Ego", "Eppy", "Noel", "Miguel", "Juju"];
+var studentRow2 = ["Jose", "Jordan", "Kike", "Greg"];
+var studentRow3 = ["Yisenia", "Alicia", "Lupe", "Evelyn"];
 
 // Create an entire classroom
 
+var EntireClass = [StudentRow1, studentRow2, studentRow3];
+
 // print out every student in  row 1
+
+for (var i = 0; i < StudentRow1.length; i++) {
+    console.log(StudentRow1[i]);
+}
 
 // print out every student in row2 that begins with the letter J
 
+for (var i = 0; i < studentRow2.length; i++) {
+    if (studentRow2[i].charAt(0) === "j") {
+        console.log(" name starts with the letter :" + studentRow2[i]);
+    }
+}
 // how to acess second students name in row3
 
 // how do we acess the third student in a row2?
