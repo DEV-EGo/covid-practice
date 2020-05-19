7b6643906beeaaa5e824a6c8b2ab863dcd6a5fc2
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-18 21:20:40 -0400

    more

--- Javascript_practice/25-loop/loop_tv.js
+++ Javascript_practice/25-loop/loop_tv.js
@@ -5,11 +5,33 @@ var Movies;
 
 for (var i = 1; i < 3; i++) {
 
-    // asking user what is there 1,2,3 fav movie
 
-    favMovies = prompt("what's your # " + i + " favorite movie ?");
+    Movies = prompt("what's your # " + i + " favorite movie ?"); // asking user what is there 1,2,3 fav movie
 
-    // adding (PUSH) the variable aka answer into favmovie array
 
-    favMovies.push(Movies);
+    favMovies.push(Movies);// adding (PUSH) the variable aka answer into favmovie array
+
+}
+
+// looping into the favMOvie array 
+
+for (var i = 0; i < favMovies.length; i++) {
+
+    alert(favMovies[i]);// create an alert that will list out each new movie tyoed in by user
+
+}
+
+// ======================MORE PRACTICE============================================================
+
+var favTVShow = [];
+var TVshowSucks;
+
+for (var i = 2; i < 4; i++) {
+    TVshowSucks = prompt(" what is your fav tv show " + i + " ?");
+
+    favTVShow.push(TVshowSucks);
+}
+
+for (var i = 0; i < favTVShow.length; i++) {
+    alert(favTVShow[i]);
 }
