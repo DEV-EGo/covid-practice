8f0261b0ba29be7084a523cae75b78151916d4d3
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-18 21:03:03 -0400

    more work

---
+++ Javascript_practice/25-loop/loop_tv.js
@@ -0,0 +1,15 @@
+var favMovies = [];
+var Movies;
+
+// FOR-Loop will repat 3x
+
+for (var i = 1; i < 3; i++) {
+
+    // asking user what is there 1,2,3 fav movie
+
+    favMovies = prompt("what's your # " + i + " favorite movie ?");
+
+    // adding (PUSH) the variable aka answer into favmovie array
+
+    favMovies.push(Movies);
+}
