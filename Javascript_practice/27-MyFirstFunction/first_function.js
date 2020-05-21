ca9931b1014caddf7f9a4d671a8e5ff9b61d3d5f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-21 14:59:04 -0400

    add-multiply

---
+++ Javascript_practice/27-MyFirstFunction/first_function.js
@@ -0,0 +1,17 @@
+function Add(x, y, z) {
+    console.log(x + y + z);
+}
+
+function AddReturn(x, y, z) {
+    return x + y + z;
+}
+
+// multiplier
+
+function Multiply(x, y, x) {
+    console.log(x * y * z);
+}
+
+function MultiplyReturn(x, y, z) {
+    return x * y * z;
+}
