6d149916ace7b50d6b90d36818f4da83d2c85aae
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-28 22:39:31 -0400

    array

---
+++ Javascript_practice/28-GoodArray/AGoodArray.js
@@ -0,0 +1,11 @@
+var jordans = [2, 3, 4, 5, 6, 7];
+
+for (var i = 0; i < jordans.length; i++) {
+    if (jordans[i] > 2) {
+
+        console.log(" thats some good retros")
+    }
+    else {
+        console.log(" these aint it");
+    }
+}
