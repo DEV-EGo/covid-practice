3f3b134f34eb9df05e9d2e57b9f7a3699ea6651f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-06-09 13:03:40 -0400

    practicing number functions

--- Javascript_practice/28-GoodArray/AGoodArray.js
+++ Javascript_practice/28-GoodArray/AGoodArray.js
@@ -1,11 +1,32 @@
 var jordans = [2, 3, 4, 5, 6, 7];
 
 for (var i = 0; i < jordans.length; i++) {
-    if (jordans[i] > 2) {
+    if (jordans[i] < 2) {
 
         console.log(" thats some good retros")
     }
     else {
         console.log(" these aint it");
+    }
+}
+
+var freedom = ["Anonymous", "NAACP", "PROTEST"];
+
+for (var f = 0; f < freedom.length; f++) {
+    if (freedom[f] < 3) {
+        console.log(" we are the resistance");
+    }
+    else {
+        console.log("system error");
+    }
+}
+
+var FindTheseNUmbers = [1, 2, 3, 4, 5, 6];
+for (var x = 0; x < FindTheseNUmbers.length; x++) {
+    if (FindTheseNUmbers[x] > 2) {
+        console.log("numbers are greater than 2");
+    }
+    else {
+        console.log("numbers are less than 2");
     }
 }
