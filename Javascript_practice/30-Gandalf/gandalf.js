38b55d8fae71c742d09df2d34c1e75b999f5dc51
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-06-12 16:53:26 -0400

    batman

--- Javascript_practice/30-Gandalf/gandalf.js
+++ Javascript_practice/30-Gandalf/gandalf.js
@@ -1,54 +1,79 @@
-var Gandalf = {
-    "real name": "gandalf",
-    "age (est)": 11000,
-    "race": "Maia",
-    "haveRetirementPlan": true,
-    "aliases": [
-        "Greyhammer",
-        "stormcrow",
-        "Mithrandir",
-        "Gnadalf the grey",
-        "Gandalf the white"
+// var Gandalf = {
+//     "real name": "gandalf",
+//     "age (est)": 11000,
+//     "race": "Maia",
+//     "haveRetirementPlan": true,
+//     "aliases": [
+//         "Greyhammer",
+//         "stormcrow",
+//         "Mithrandir",
+//         "Gnadalf the grey",
+//         "Gandalf the white"
+//     ]
+// };
+
+// alert("my name is " + Gandalf["real name"]);
+
+// if (Gandalf.haveRetirementPlan) {
+//     var age = "age (est)";
+//     var years = Gandalf[age];
+
+//     alert(" my 401k has been steady stacking for " + years + "years");
+// }
+
+// alert("i have more than " + Gandalf.aliases.length + " aliases ");
+
+// alert(" my designation " + Gandalf["designation"]);
+
+// // more practice
+
+// var anonymous = {
+//     "real name": " anonymous",
+//     "age": 0,
+//     "race": "multiRacial",
+//     "retire": false,
+//     "aliases": [
+//         "The people",
+//         "Antifa",
+//         "Terrorist group",
+//         "ANON"
+//     ]
+// };
+
+// alert("we are" + anonymous["real name"]);
+
+// if (anonymous.retire) {
+//     var age = "age";
+//     var years = anonymous["age"];
+
+//     alert("my 401k IS NOT STACKING" + years + "years");
+// }
+
+// alert("we have more that one name " + anonymous.aliases.length + " aliases");
+
+// alert(" my destination " + anonymous["destination"]);
+
+// more 
+
+var Batman = {
+
+    "real name": "Bruce Wayne",
+    "Age": "27",
+    "Job": "Vigilante",
+    "race": "Human",
+    "Aliases": [
+        "The Dark Knight",
+        "Bats",
+        "ManBat"
     ]
 };
 
-alert("my name is " + Gandalf["real name"]);
+alert(" i am " + Batman["real name"]);
 
-if (Gandalf.haveRetirementPlan) {
-    var age = "age (est)";
-    var years = Gandalf[age];
+if (Batman.Job) {
+    var job = "Job";
+    var CrimeStoper = Batman["Job"];
 
-    alert(" my 401k has been steady stacking for " + years + "years");
+    alert(" i am known for being a " + CrimeStoper);
+
 }
-
-alert("i have more than " + Gandalf.aliases.length + " aliases ");
-
-alert(" my designation " + Gandalf["designation"]);
-
-// more practice
-
-var anonymous = {
-    "real name": " anonymous",
-    "age": 0,
-    "race": "multiRacial",
-    "retire": false,
-    "aliases": [
-        "The people",
-        "Antifa",
-        "Terrorist group",
-        "ANON"
-    ]
-};
-
-alert("we are" + anonymous["real name"]);
-
-if (anonymous.retire) {
-    var age = "age";
-    var years = anonymous["age"];
-
-    alert("my 401k IS NOT STACKING" + years + "years");
-}
-
-alert("we have more that one name " + anonymous.aliases.length + " aliases");
-
-alert(" my destination " + anonymous["destination"]);
