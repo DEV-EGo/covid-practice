2e49ed6a05dd8557762973f5da87dd11248b615d
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-06-21 20:42:20 -0400

    new work

--- Javascript_practice/31-MyFirstObject/cars.js
+++ Javascript_practice/31-MyFirstObject/cars.js
@@ -1,29 +1,31 @@
-var Car = {
-    make: " GT86",
-    Color: "Khaki",
-    Miles: "56000",
-    IsWorking: true,
+var Vehicle = {
+    make: "Toyota",
+    model: "GT86 ",
+    color: "dessert khaki",
+    mileage: 50000,
+    IsDriveable: true,
 
     DriveToWork: function () {
-
-        alert(" Old Miles: " + this.Miles);
-
-        this.Miles = this.Miles + 5;
-
-        alert(" after the test drive it gained " + this.Miles);
-
+        alert(" old mileage:" + this.mileage);
+        this.mileage = this.mileage + 20;
+        alert(" New Miles" + this.mileage);
     },
 
     DriveAroundTheWorld: function () {
+        alert("old miles" + this.mileage);
+        this.mileage = this.mileage + 1000;
 
-        alert(" old miles : " + this.Miles);
+        alert("new miles are" + this.mileage);
+        alert(" your car needs a tune up");
+        this.IsDriveable = false;
+    },
 
-        this.Miles = this.Miles + 24000;
-        alert(" New Miles: " + this.Miles);
-        alert(" Your car needs a tune up! ");
+    GetAtuneUp: function () {
+        alert("car is ready to race");
+        this.IsDriveable = true;
+    },
 
-        this.IsWorking = false;
+    Honk: function () {
+        alert(" HONK ! HONK!");
     }
-
-}
-
+};
