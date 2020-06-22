7241927c7b4808fefddf3de8f38727277a6e3b04
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-06-21 22:38:30 -0400

    new

---
+++ Javascript_practice/31-MyFirstObject/MorePractice.js
@@ -0,0 +1,38 @@
+var Tuner = {
+    Vehicle: " Toyota",
+    Model: "Supra",
+    color: "Orange",
+    Miles: 20000,
+    Drives: true,
+
+    // creating drive to work method
+    // methods are functions associated with the object
+
+    Race: function () {
+
+        // "this" reffering to (this.miles) refers to the object the method is a part of
+
+        alert(" Current miles" + this.Miles);
+
+        // adding 8 to the car miles
+        this.Miles = this.Miles + 8;
+
+        // alert the new cars miles
+
+        alert(" new miles" + this.Miles);
+    },
+
+    // DriveAroundtheblock method adds 20000 miles to the car, sets "Drives" to false and makes alerts
+    DriveAroundTheBlock: function () {
+        alert("old miles" + this.Miles);
+
+        this.Miles = this.Miles + 20000;
+
+        alert("new Miles+" + this.Miles);
+        alert(" your car needs to hit the shop ");
+
+        this.Drives = false;
+    },
+
+
+}
