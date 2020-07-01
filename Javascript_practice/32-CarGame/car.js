79b1e3ca12e203d8f33a30631e0155cbb38f2c05
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-01 13:51:01 -0400

    new js

---
+++ Javascript_practice/32-CarGame/car.js
@@ -0,0 +1,11 @@
+var Vehicle = {
+    Make: "NISSAN",
+    Mode: "GTR",
+    Color: "Silver",
+    Miles: 10000,
+    IsDriving: true,
+
+    DriveToWork: function () {
+        alert(" you have this many miles" + this.Miles);
+    }
+}
