f549500c151c82e878853c0c291210c6a8d53080
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-01 18:59:08 -0400

    racecar1

--- Javascript_practice/32-CarGame/new/extraCar.js
+++ Javascript_practice/32-CarGame/new/extraCar.js
@@ -3,16 +3,16 @@ var NewCar = {
     Model: " f50",
     Color: " Red",
     Miles: 1000,
-    IsRaceAbel: true,
+    IsRaceAble: true,
 
-    StartEngine = function () {
+    StartEngine: function () {
         alert(" Start Your Engine" + this.Miles);
         this.Miles = this.Miles;
 
         alert(" You ran this many miles " + this.Miles);
     },
 
-    LapAroundTheTrack = function () {
+    LapAroundTheTrack: function () {
         alert(" you ran these many miles around the track" + this.Miles);
 
         this.Miles = this.Miles + 3000;
@@ -20,5 +20,25 @@ var NewCar = {
         alert(" Engine is hot you ran" + this.Miles + " Miles Per hour");
 
         alert(" Pull into the Pit Stop your engine is running hot");
+    },
+
+    PitStop: function () {
+        alert(" You're good to go Win this race");
+        this.IsRaceAble = true;
+    },
+
+    Turbo: function () {
+        alert(" TURBO KICKIN IN! PSSSHHHHHHHHHH!!! ");
     }
+};
+
+Function clearUpStats() {
+
+    console.log("Make" + NewCar.Make);
+    console.log("Model" + NewCar.Model);
+    console.log("Color" + NewCar.Color);
+    console.log("Miles" + NewCar.Miles);
+    console.log("Ready to race" + NewCar.IsRaceAble);
+    console.log("===============lets ride =================");
+
 }
