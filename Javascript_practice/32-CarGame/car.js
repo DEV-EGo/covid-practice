48d4e5b997377291ac1de49f479de8a049f980b6
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-01 14:12:54 -0400

    car

--- Javascript_practice/32-CarGame/car.js
+++ Javascript_practice/32-CarGame/car.js
@@ -1,7 +1,7 @@
 var Vehicle = {
-    Make: "NISSAN",
-    Mode: "GTR",
-    Color: "Silver",
+    Make: " NISSAN",
+    Mode: " GTR",
+    Color: " Silver",
     Miles: 10000,
     IsDriving: true,
 
@@ -18,7 +18,7 @@ var Vehicle = {
 
         this.Miles = this.Miles + 30000;
 
-        alert("new miles" + this.Miles);
+        alert(" new miles" + this.Miles);
         alert(" your car needs a tune up!");
 
         this.IsDriving = false;
@@ -38,11 +38,47 @@ var Vehicle = {
 // log all of the cars currrent stats in the console
 
 function DoOverStats() {
-    console.log("Make" + Vehicle.Make);
-    console.log("Model" + Vehicle.Mode);
-    console.log("Color" + Vehicle.Color);
-    console.log("Miles" + Vehicle.Miles);
-    console.log("is driveable" + Vehicle.IsDriving);
+    console.log(" Make" + Vehicle.Make);
+    console.log(" Model" + Vehicle.Mode);
+    console.log(" Color" + Vehicle.Color);
+    console.log(" Miles" + Vehicle.Miles);
+    console.log(" is driveable" + Vehicle.IsDriving);
     console.log(" ============================ ");
 
 }
+
+// main process
+
+// capture keyboard input by the user
+
+document.onkeyup = function (event) {
+
+    var letterOnClick = event.key.toLowerCase();
+
+    // if the letter is "H" run this code
+
+    if (letterOnClick === "h") {
+        Vehicle.honk();
+        DoOverStats();
+    }
+
+    // if the letter is "D" run this code
+
+    if (letterOnClick === "d") {
+        Vehicle.DriveToWork();
+        DoOverStats();
+    }
+
+    // if the letter is "W" run this code
+    if (letterOnClick === "w");
+    Vehicle.DriveAroundTheBlock();
+    DoOverStats();
+
+    // if the letter is "T" run this code
+
+    if (letterOnClick === "T"); {
+        Vehicle.TuneUpTheCar();
+        DoOverStats();
+    }
+
+};
