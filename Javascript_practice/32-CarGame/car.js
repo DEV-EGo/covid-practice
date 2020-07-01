730318ef95e5a7f42c4c6b930d0ac81266387742
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-01 13:54:56 -0400

    js

--- Javascript_practice/32-CarGame/car.js
+++ Javascript_practice/32-CarGame/car.js
@@ -7,5 +7,25 @@ var Vehicle = {
 
     DriveToWork: function () {
         alert(" you have this many miles" + this.Miles);
+
+        this.Miles = this.Miles;
+
+        alert(" new miles" + this.Miles);
+    },
+
+    DriveAroundTheBlock: function () {
+        alert(" You put these many miles" + this.Miles);
+
+        this.Miles = this.Miles + 30000;
+
+        alert("new miles" + this.Miles);
+        alert(" your car needs a tune up!");
+
+        this.IsDriving = false;
+    },
+
+    TuneUpTheCar: function () {
+        alert(" Dump the clutch and let it roll! ");
+        this.IsDriving = true;
     }
 }
