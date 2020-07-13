864b18376e6804f54fa755dc5d1acf590353bfea
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-13 14:32:30 -0400

    jquery learnig

--- JQUERY/02-JSDrinklist/js/ai.js
+++ JQUERY/02-JSDrinklist/js/ai.js
@@ -5,3 +5,25 @@ var $div = $("testing");
 $div.html("etc");
 
 $("#testing").append("this is working");
+
+
+("====================");
+
+
+$("#newTest").html("<h1> this is a new test");
+
+var $newDiv = $("newTest");
+
+$newDiv.html("yeh");
+
+$("#newTest").append(" Alert test");
+
+("====================");
+
+$("#helloDiv").html("<h1> hello world </h1>");
+
+var $helloDiv = $("div");
+
+$helloDiv.html("hello");
+
+$("#helloDiv").append(" this is a new hello world");
