33036c28276423ba3381bbfee2d34db91b3eee85
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-13 14:38:53 -0400

    bugs

--- JQUERY/02-JSDrinklist/js/ai.js
+++ JQUERY/02-JSDrinklist/js/ai.js
@@ -1,10 +1,10 @@
 $("#testing").html("<h1> testing</h1>");
 
-var $div = $("testing");
+var $div = $("<testing>");
 
 $div.html("etc");
 
-$("#testing").append("this is working");
+$("#testing").append($div);
 
 
 ("====================");
@@ -12,18 +12,18 @@ $("#testing").append("this is working");
 
 $("#newTest").html("<h1> this is a new test");
 
-var $newDiv = $("newTest");
+var $newDiv = $("<newTest>");
 
 $newDiv.html("yeh");
 
-$("#newTest").append(" Alert test");
+$("#newTest").append($newDiv);
 
 ("====================");
 
-$("#helloDiv").html("<h1> hello world </h1>");
+$("#helloDiv").html("<title> hello world <title>");
 
-var $helloDiv = $("div");
+var $helloDiv = $("<div>");
 
 $helloDiv.html("hello");
 
-$("#helloDiv").append(" this is a new hello world");
+$("#helloDiv").append($helloDiv);
