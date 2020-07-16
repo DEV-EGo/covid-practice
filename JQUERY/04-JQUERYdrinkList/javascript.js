34f535c8081aad0763853dafbfc86a0fbc429379
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-15 21:13:04 -0400

    cartoonlist

--- JQUERY/04-JQUERYdrinkList/javascript.js
+++ JQUERY/04-JQUERYdrinkList/javascript.js
@@ -5,3 +5,27 @@ var drinklist = [
     "ice coffee: $2",
     "ice latter: $4"
 ];
+
+var drinkDiv = $("#drink-options");
+
+for (var i = 0; i < drinklist.length; i++) {
+
+    var newdrink = $("<div>") + drinklist[i] + ("</div>");
+
+    drinkDiv.append(newdrink);
+}
+
+var cartoon = [
+    "avatar",
+    "DBZ",
+    "NARUTO",
+    "GUNDAM",
+    "Samurai jack"
+];
+
+var cartoonList = $("#anime");
+
+for (var i = 0; i < cartoon.length; i++) {
+    var cartoons = $("<div>") + cartoon[i] + ("</div>");
+    cartoonList.append(cartoons);
+}
