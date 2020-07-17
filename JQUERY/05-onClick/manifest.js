30ceec20946c4ad40fade8c9710ef883c64d2f01
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-17 12:58:09 -0400

    making buttons work

---
+++ JQUERY/05-onClick/manifest.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#click-me").on("click", function () {
+        alert("you got mail!");
+    });
+
+    $("#nextClick").on("click", function () {
+        alert("touch me");
+    });
+
+    $("#thisButton").on("click", function () {
+        alert("bingo");
+    });
+});
+
