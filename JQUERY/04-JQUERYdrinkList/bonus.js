0acb6ecf66c4d7c4fe4319b3ae36dd828537e676
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-17 02:01:02 -0400

    more practice

--- JQUERY/04-JQUERYdrinkList/bonus.js
+++ JQUERY/04-JQUERYdrinkList/bonus.js
@@ -11,3 +11,18 @@ var ClosetDiv = $("#bonus");
 $.each(shoes, function (i, kicks) {
     ClosetDiv.append("<div>" + kicks + "</div>");
 });
+
+var clothes = [
+    "BAPE",
+    "SUPREME",
+    "KITH",
+    "BALENCIAGA",
+    "VLONE"
+];
+
+var clothing = $("#clothing");
+
+for (var i = 0; i < clothes.length; i++) {
+    var clothesStorage = ("<div>" + clothes[i] + "</div>");
+    clothing.append(clothesStorage);
+}
