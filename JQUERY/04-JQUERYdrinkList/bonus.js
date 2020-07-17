b126751220fb52bf53a983f75a45b3fbafd38c7f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-17 12:43:27 -0400

    add more practivce

--- JQUERY/04-JQUERYdrinkList/bonus.js
+++ JQUERY/04-JQUERYdrinkList/bonus.js
@@ -26,3 +26,35 @@ for (var i = 0; i < clothes.length; i++) {
     var clothesStorage = ("<div>" + clothes[i] + "</div>");
     clothing.append(clothesStorage);
 }
+
+var fortunefive = [
+    "Apple",
+    "Microsoft",
+    "bmw",
+    "TESLA",
+    "NIKE"
+];
+
+var millions = $("#million");
+
+for (var i = 0; i < fortunefive.length; i++) {
+    var bucks = ("<div>" + fortunefive[i] + "</div");
+    millions.append(bucks);
+}
+
+var food = [
+    "tacos",
+    "burritos",
+    "burgers",
+    "hot dogs",
+    "rice",
+    "soup"
+];
+
+var DigIn = $("#Grub");
+
+for (var i = 0; i < food.length; i++) {
+
+    var comida = ("<div>" + food[i] + "</div>");
+    DigIn.append(comida);
+}
