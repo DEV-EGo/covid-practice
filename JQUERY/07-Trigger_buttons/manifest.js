ebfd82520c3b10687a92dae86fcef3ebd4ea797f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-18 21:35:56 -0400

    finished

--- JQUERY/07-Trigger_buttons/manifest.js
+++ JQUERY/07-Trigger_buttons/manifest.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     $("#random-button").on("click", function () {
         var random = Math.floor(Math.random() * 1000) + 1;
-
-        $("#random-number").text(random);
+        $("#random-button").text(random);
     });
+
+    $("#randy-button").on("click", function () {
+        var randomize = Math.floor(Math.random() * 1000) + 1;
+        $("#randy-button").text(randomize);
+    });
+
 });
