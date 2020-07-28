3e3cb41e32b32d48eb6bc6c8429dc5714d146c3e
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-07-28 19:02:28 -0400

    logic

--- JQUERY/12-crystalExample/manifest.js
+++ JQUERY/12-crystalExample/manifest.js
@@ -15,6 +15,15 @@
 //     alert(" you got a ring! " + counter + " up!");
 // });
 
+// establishing the target number (setting to 50)
+
+var scoreTarget = 50;
+
+// setting the "number-limit" header to match the "scoreTarget"
+
+$("#number-limit").text(scoreTarget);
+
+
 // this new counter below will increase by *10* every time the image is clicked
 // then the display will show the value of the new score
 
@@ -22,4 +31,15 @@ var ringcounter = 0;
 $(".Sonic_ring").on("click", function () {
     ringcounter += 10;
     alert(" Double XP points " + ringcounter + " UP! ");
+
+    // creating logic that will "check" to see if the click counter matches the score limit
+    // each click event will be triggered by each click
+    // each click will be increased by 10 and re-evaluated
+
+    if (ringcounter === scoreTarget) {
+
+        // creating a celebration pop up
+
+        alert(" YOU WIN !");
+    }
 });
