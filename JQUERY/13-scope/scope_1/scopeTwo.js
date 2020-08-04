edfea2ee2311c5ece2e082609306ac53b9424b53
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-08-04 13:14:28 -0400

    scope_2

--- JQUERY/13-scope/scope_1/scopeTwo.js
+++ JQUERY/13-scope/scope_1/scopeTwo.js
@@ -4,21 +4,24 @@ var cat = {
     Age: 10,
 
     DisplayCatInfo: function () {
+
         console.log(" inside the displayCatInfo function ");
 
         console.log(this);
 
-        console.log("Name", this.name, "Color:", this.color, "Age", this.Age);
+        console.log("Name:", this.name, "Color:", this.color, "Age:", this.Age);
 
         var NestedFunctionInside = function () {
             console.log("this is the nested function inside 1st function");
 
             console.log(this);
 
-            console.log("name", this.name, "color", this.color, "Age", this.Age);
+            console.log("name:", this.name, "color:", this.color, "Age:", this.Age);
 
         };
         NestedFunctionInside();
 
     }
 };
+
+cat.DisplayCatInfo();
