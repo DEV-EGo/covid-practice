e3b8ac1adbdea7f388c9ee242c6204cbbfcbaf69
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-08-17 18:42:28 -0400

    work_in_progress

---
+++ JQUERY/19-CobbWeb_Activity/javascript/manifest.js
@@ -0,0 +1,41 @@
+var CobbWeb = {
+
+    MajorWeb: {
+
+        items: " sneakers ",
+
+        BigWebInsideMajorWeb: {
+
+            items:
+                [
+                    " Supreme BOGO ",
+                    " BAPE Hoodie ",
+                    " OFF-WHITE "
+                ],
+
+            SmallWebInsideMajorWeb: {
+
+                items:
+                    " NewEraCap "
+                ,
+
+                tinywebinMajorWeb: {
+
+                    items: [
+                        "BOGO Beanie",
+                        "BAPESTA",
+                        "RayBans"
+                    ]
+                }
+
+            },
+            OtherBigweb:
+            {
+                items: " headphones "
+            }
+        }
+    }
+};
+
+console.log(" i found my" + CobbWeb.MajorWeb.BigWebInsideMajorWeb.items[0]);
+console.log("i found my" + CobbWeb.MajorWeb.BigWebInsideMajorWeb.SmallWebInsideMajorWeb.items + "!");
