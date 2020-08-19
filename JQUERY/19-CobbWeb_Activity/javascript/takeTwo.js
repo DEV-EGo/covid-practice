1ddc327b3d06bda1eeb6b9892fe0ea5ac3b3658e
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-08-19 13:36:40 -0400

    add

--- JQUERY/19-CobbWeb_Activity/javascript/takeTwo.js
+++ JQUERY/19-CobbWeb_Activity/javascript/takeTwo.js
@@ -1,6 +1,10 @@
 var hustle = {
 
-    money: " Rich Porter ",
+    money: [
+        " Rich Porter",
+        " Alpo Martinez",
+        " Aiz aka Ace"
+    ],
 
     alpo: {
 
@@ -37,16 +41,50 @@ var hustle = {
             "Rims",
             "Clothes"
         ]
+    },
+
+    LifeOfCrime: {
+
+        jail: [
+            " Murder",
+            " Extortion",
+            " Man Slaughter"
+        ]
+    },
+
+    Movie: {
+
+        lifeStory: [
+
+            "Paid N Full"
+        ]
+    },
+
+    Music: {
+
+        Rappers: [
+            " Camron",
+            " Future",
+            " Nas",
+            " Drake"
+        ]
     }
 
 };
 
-console.log(" kings of new york" + hustle.money);
+console.log("This was based on the biopic " + hustle.Movie.lifeStory);
 
-console.log(" You can find him in " + hustle.alpo.work);
+console.log("Narc kings of new york" + hustle.money);
 
-console.log(" im broke baby - " + hustle.alpo.AZ.work);
 
-console.log(" Alpo wacked the real " + hustle.alpo.MoneyMitch.work);
+console.log("Drug empire expanded from " + hustle.alpo.work);
 
-console.log(" We spent it all on " + hustle.theBag.luxury);
+console.log("im broke baby -" + hustle.alpo.AZ.work);
+
+console.log("Alpo wacked the real " + hustle.alpo.MoneyMitch.work);
+
+console.log("Every penny was spent on " + hustle.theBag.luxury);
+
+console.log("NYPD gathered enought evidence to convict these kingpin's with" + hustle.LifeOfCrime.jail);
+
+console.log("Artist like:" + hustle.Music.Rappers + " imortalized their life story in their music");
