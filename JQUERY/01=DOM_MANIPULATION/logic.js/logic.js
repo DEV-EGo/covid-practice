ecfd2a9b3623275846aa4ef3e3e514aac06a4548
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-09-18 13:12:04 -0400

    add

---
+++ JQUERY/01=DOM_MANIPULATION/logic.js/logic.js
@@ -0,0 +1,15 @@
+var TargetID = document.getElementById("empty-div");
+
+var SecondT = document.getElementById("second-div");
+
+var ThirdT = document.getElementById("third-div");
+
+var FourthT = document.getElementById("fourth-div");
+
+var FifthT = document.getElementById("fifth-div");
+
+var SixthT = document.getElementById("sixth-div");
+
+var SeventhT = document.getElementById("seventh-div");
+
+var Eight = document.getElementById("eight-div");
