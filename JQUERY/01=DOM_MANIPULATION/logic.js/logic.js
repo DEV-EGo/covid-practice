f011abec759facb775b5bbe4a2db8a535999a75e
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-09-18 20:13:26 -0400

    work

--- JQUERY/01=DOM_MANIPULATION/logic.js/logic.js
+++ JQUERY/01=DOM_MANIPULATION/logic.js/logic.js
@@ -13,3 +13,28 @@ var SixthT = document.getElementById("sixth-div");
 var SeventhT = document.getElementById("seventh-div");
 
 var Eight = document.getElementById("eight-div");
+
+// ========use the JS method ".textcontent" to change the content on screen and out put "hello world"
+
+// SecondT.textContent = "hello world";
+// ThirdT.textContent = "Third Target Being called";
+// FifthT.textContent = "5th target being called";
+
+// adding to the DIV using the same. textcontent method
+// while replacing the old content
+
+var NewElementDiv = document.createElement("Div");
+var ElementFour = doucment.createElement("Cuatro-elemento");
+var ElementFive = document.createElement("Cinco-element");
+var ElementSix = document.createElement("Seix-element");
+var ElementSeven = document.createElement("Siete-element");
+var ElementEight = document.createElement("Ocho-element");
+
+// adding text inside new elements created^^
+
+NewElementDiv.textContent = " hey ";
+ElementFour.textContent = " cuatro";
+ElementFive.textContent = " cinco ";
+ElementSix.textContent = " seis ";
+ElementSix.textContent = " siete ";
+ElementEight.textContent = " ocho ";
