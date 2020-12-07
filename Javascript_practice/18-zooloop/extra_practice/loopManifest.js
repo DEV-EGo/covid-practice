3408922c23b2c683c4818193614055c7a108d10f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-07 13:08:02 -0500

    roboto js practice

--- Javascript_practice/18-zooloop/extra_practice/loopManifest.js
+++ Javascript_practice/18-zooloop/extra_practice/loopManifest.js
@@ -1,15 +1,45 @@
-var teams = [
-    "yankees",
-    "Dodgers",
-    "ManchesterUNTD",
-    "RealMadrid"
+var gamesystems=[
+    "ps5",
+    "switch",
+    "ps4",
+    "oculus"
 ];
-
-function DisplayThisArray(teams){
-    for (var i=0; i < teams.lenght; i++){
-        console.log(teams[i]);
+function Gamelist(pickup){
+    for (var i=0; i<pickup.length; i++){
+        console.log(pickup[i]);
     }
 }
-DisplayThisArray(teams);
-    
-console.log("==========");
+Gamelist(gamesystems);
+console.log("=========more practice======");
+
+var lunchtime=[
+    "chick-filA",
+    "Chinese",
+    "subway",
+    "mexican food",
+    "burger king"
+];
+
+function letsEat(munchies){
+    for (var i=0; i<munchies.length;i++){
+        console.log(munchies[i]);
+    }
+}
+
+letsEat(lunchtime);
+console.log("==========more practice=====");
+
+var roboto =[
+    "eat",
+    "sleep",
+    "code",
+    "destroy"
+];
+
+function irobot(ai){
+    for (var x=0; x<ai.length;x++){
+        console.log(ai[x]);
+    }
+}
+irobot(roboto);
+console.log("==========more practice=====");
