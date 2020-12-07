8f605e0e259d9d1eb54ceca43cd67c949382af03
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-07 12:57:35 -0500

    more practice

---
+++ Javascript_practice/18-zooloop/extra_practice/loopManifest.js
@@ -0,0 +1,15 @@
+var teams = [
+    "yankees",
+    "Dodgers",
+    "ManchesterUNTD",
+    "RealMadrid"
+];
+
+function DisplayThisArray(teams){
+    for (var i=0; i < teams.lenght; i++){
+        console.log(teams[i]);
+    }
+}
+DisplayThisArray(teams);
+    
+console.log("==========");
