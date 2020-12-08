d9535454f24a575c7d89fec36fbde37514f1347d
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-08 13:27:46 -0500

    practice day2

--- Javascript_practice/18-zooloop/extra_practice/loopManifest.js
+++ Javascript_practice/18-zooloop/extra_practice/loopManifest.js
@@ -43,3 +43,111 @@ function irobot(ai){
 }
 irobot(roboto);
 console.log("==========more practice=====");
+
+var drinks =[
+    "sweat tea",
+    "coca-cola",
+    "pepsi",
+    "dr Pepper",
+    "sprite"
+];
+
+function LetsDrink(sipping){
+    for(var s=0;s<sipping.length;s++){
+        console.log(sipping[s]);
+    }
+}
+LetsDrink(drinks);
+console.log("==========more practice=====");
+
+var Nintendo =[
+    "switch",
+    "Gameboy",
+    "Gameboy Advanced",
+    "Nintendo64"
+];
+
+function letsplay(Games){
+    for(var n=0;n<Games.length;n++){
+        console.log(Games[n]);
+    }
+}
+letsplay(Nintendo);
+console.log("==========more practice=====");
+
+var heros =[
+    "SuperMario",
+    "DOOM Guy",
+    "Kirby",
+    "Yoshi"
+];
+
+function JapaneseHeros(NintendoClassic){
+    for(var t=0;t<NintendoClassic.length;t++){
+        console.log(NintendoClassic[t])
+    }
+}
+JapaneseHeros(heros);
+console.log("==========more practice=====");
+
+var jobs=[
+    "RGX",
+    "IA",
+    "Full StackDev",
+    "JhonnysAuto"
+];
+
+function hustle(Cashflow){
+    for(var p=0;p<Cashflow.length;p++){
+        console.log(Cashflow[p]);
+    }
+}
+hustle(jobs);
+console.log("==========more practice=====");
+
+var cards=
+[
+    "Yu-Gi-Oh",
+    "Pokemon",
+    "Digimon",
+    "UNO"
+];
+
+function LetsDuel(Deck){
+    for(var y=0;y<Deck.length;y++){
+        console.log(Deck[y]);
+    }
+}
+
+LetsDuel(cards);
+console.log("==========more practice=====");
+
+var cities=
+[
+    "NYC",
+    "Chicago",
+    "LA",
+    "Charlotte",
+    "Miami"
+];
+function vacation(trip){
+    for(var v=0;v<trip.length;v++){
+        console.log(trip[v]);
+    }
+}
+vacation(cities);
+console.log("==========more practice=====");
+
+var tech=[
+    "JS",
+    "Java",
+    "ReactJS",
+    "Ruby on Rails"
+];
+function Learn(NewLibrarys){
+    for (var c=0;c<NewLibrarys.length;c++){
+        console.log(NewLibrarys[c]);
+    }
+}
+Learn(tech);
+console.log("==========more practice=====");
