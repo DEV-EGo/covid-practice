a0995d378dbfe60234bbd34ccaf581ecbed3901a
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-08 22:06:19 -0500

    arrays

--- Javascript_practice/18-zooloop/extra_practice/loopManifest.js
+++ Javascript_practice/18-zooloop/extra_practice/loopManifest.js
@@ -1,229 +1,289 @@
-var gamesystems=[
-    "ps5",
-    "switch",
-    "ps4",
-    "oculus"
-];
-function Gamelist(pickup){
-    for (var i=0; i<pickup.length; i++){
-        console.log(pickup[i]);
-    }
-}
-Gamelist(gamesystems);
-console.log("=========more practice======");
-
-var lunchtime=[
-    "chick-filA",
-    "Chinese",
-    "subway",
-    "mexican food",
-    "burger king"
-];
-
-function letsEat(munchies){
-    for (var i=0; i<munchies.length;i++){
-        console.log(munchies[i]);
-    }
-}
-
-letsEat(lunchtime);
-console.log("==========more practice=====");
-
-var roboto =[
-    "eat",
-    "sleep",
-    "code",
-    "destroy"
-];
-
-function irobot(ai){
-    for (var x=0; x<ai.length;x++){
-        console.log(ai[x]);
-    }
-}
-irobot(roboto);
-console.log("==========more practice=====");
-
-var drinks =[
-    "sweat tea",
-    "coca-cola",
-    "pepsi",
-    "dr Pepper",
-    "sprite"
-];
-
-function LetsDrink(sipping){
-    for(var s=0;s<sipping.length;s++){
-        console.log(sipping[s]);
-    }
-}
-LetsDrink(drinks);
-console.log("==========more practice=====");
-
-var Nintendo =[
-    "switch",
-    "Gameboy",
-    "Gameboy Advanced",
-    "Nintendo64"
-];
-
-function letsplay(Games){
-    for(var n=0;n<Games.length;n++){
-        console.log(Games[n]);
-    }
-}
-letsplay(Nintendo);
-console.log("==========more practice=====");
-
-var heros =[
-    "SuperMario",
-    "DOOM Guy",
-    "Kirby",
-    "Yoshi"
-];
-
-function JapaneseHeros(NintendoClassic){
-    for(var t=0;t<NintendoClassic.length;t++){
-        console.log(NintendoClassic[t])
-    }
-}
-JapaneseHeros(heros);
-console.log("==========more practice=====");
-
-var jobs=[
-    "RGX",
-    "IA",
-    "Full StackDev",
-    "JhonnysAuto"
-];
-
-function hustle(Cashflow){
-    for(var p=0;p<Cashflow.length;p++){
-        console.log(Cashflow[p]);
-    }
-}
-hustle(jobs);
-console.log("==========more practice=====");
-
-var cards=
-[
-    "Yu-Gi-Oh",
-    "Pokemon",
-    "Digimon",
-    "UNO"
-];
-
-function LetsDuel(Deck){
-    for(var y=0;y<Deck.length;y++){
-        console.log(Deck[y]);
-    }
-}
-
-LetsDuel(cards);
-console.log("==========more practice=====");
-
-var cities=
-[
-    "NYC",
-    "Chicago",
-    "LA",
-    "Charlotte",
-    "Miami"
-];
-function vacation(trip){
-    for(var v=0;v<trip.length;v++){
-        console.log(trip[v]);
-    }
-}
-vacation(cities);
-console.log("==========more practice=====");
-
-var tech=[
-    "JS",
-    "Java",
-    "ReactJS",
-    "Ruby on Rails"
-];
-function Learn(NewLibrarys){
-    for (var c=0;c<NewLibrarys.length;c++){
-        console.log(NewLibrarys[c]);
-    }
-}
-Learn(tech);
-console.log("==========more practice=====");
-
-var Ride=[
-    "Ferrari",
-    "McLaren",
-    "GTR",
-    "Porsche",
-    "Lambo"
-];
-
-function Whip(Foreign){
-    for (var e=0;e<Foreign.length;e++){
-        console.log(Foreign[e])
-    }
-}
-Whip(Ride);
-console.log("==========more practice=====");
-
-var Skills=[
-    "Weld",
-    "Code",
-    "Paint",
-    "Sneaker Resell",
-    "Stocks",
-    "Crypto"
-];
-function Money(GetPaid){
-    for(var u=0;u<GetPaid.length;u++){
-        console.log("i get paid by "+ GetPaid[u])
-    }
-}
-Money(Skills);
-console.log("==========more practice=====");
-
-var movies=[
-    "Godzilla",
-    "Kong",
-    "The Matrix",
-    "The Wolf of Wallstreet"
-];
-function Tunein(Blocbusters){
-    for (var w=0;w<Blocbusters.length;w++){
-        console.log(Blocbusters[w]+" is the best movie ever! ");
-    }
-}
-Tunein(movies);
-console.log("==========more practice=====");
-
-var christmas=[
-    "Gracie",
-    "Mom",
-    "Dad",
-    "Jordan"
-];
-
-function Gifts(Shopping){
-    for(var d=0;d<Shopping.length;d++){
-        console.log(Shopping[d]+" needs a gift");
-    }
-}
-Gifts(christmas);
-console.log("==========more practice=====");
-
-var firstpersonShooter=[
+var playstation =[
+    "God Of War",
+    "Fortnite",
     "Call of Duty",
-    "Resident Evil",
-    "Medal Of Honor",
-    "DOOM"
+    "Crash",
+    "Resident Evil"
 ];
-
-function ShootEmUp(Glock){
-    for(var q=0;q<Glock.lenght;q++){
-        console.log(Glock[q] + " Lock & Load");
+function GamestoPlay(GameLibrary){
+    for (var i=0;i < GameLibrary.length;i++){
+        console.log(GameLibrary[i] + " is the best Game on the PS5")
     }
 }
-ShootEmUp(firstpersonShooter);
-console.log("==========more practice=====");
+GamestoPlay(playstation);
+console.log("<....======practice======....>")
+
+var laptop=[
+    "MAC",
+    "Windows",
+    "Alien Ware"
+]
+function Computers(OperatingSystem){
+    for(var o=0;o<OperatingSystem.length;o++){
+        console.log(" i own this computer" + OperatingSystem[o])
+    }
+}
+Computers(laptop);
+console.log("<....======practice======....>")
+
+var space=[
+    "Mars",
+    "Venus",
+    "Niburu",
+    "Neptune",
+    "Saturn"
+];
+
+function Planets(SolarSystem){
+    for(var p=0;p<SolarSystem.length;p++){
+        console.log(SolarSystem[p] + " is our nearest neighbor in space")
+    }
+}
+Planets(space);
+console.log("<....======practice======....>")
+
+var Ancients=[
+    "Aztec",
+    "Egyptian",
+    "Maya",
+];
+
+function Culture(Civilization){
+    for(var a=0; a<Civilization.length;a++){
+        console.log(Civilization[a] + " was an advance group of ppl");
+    }
+}
+Culture(Ancients);
+console.log("<....======practice======....>")
+
+console.log("<....======practice======....>")
+
+// var gamesystems=[
+//     "ps5",
+//     "switch",
+//     "ps4",
+//     "oculus"
+// ];
+// function Gamelist(pickup){
+//     for (var i=0; i<pickup.length; i++){
+//         console.log(pickup[i]);
+//     }
+// }
+// Gamelist(gamesystems);
+// console.log("=========more practice======");
+
+// var lunchtime=[
+//     "chick-filA",
+//     "Chinese",
+//     "subway",
+//     "mexican food",
+//     "burger king"
+// ];
+
+// function letsEat(munchies){
+//     for (var i=0; i<munchies.length;i++){
+//         console.log(munchies[i]);
+//     }
+// }
+
+// letsEat(lunchtime);
+// console.log("==========more practice=====");
+
+// var roboto =[
+//     "eat",
+//     "sleep",
+//     "code",
+//     "destroy"
+// ];
+
+// function irobot(ai){
+//     for (var x=0; x<ai.length;x++){
+//         console.log(ai[x]);
+//     }
+// }
+// irobot(roboto);
+// console.log("==========more practice=====");
+
+// var drinks =[
+//     "sweat tea",
+//     "coca-cola",
+//     "pepsi",
+//     "dr Pepper",
+//     "sprite"
+// ];
+
+// function LetsDrink(sipping){
+//     for(var s=0;s<sipping.length;s++){
+//         console.log(sipping[s]);
+//     }
+// }
+// LetsDrink(drinks);
+// console.log("==========more practice=====");
+
+// var Nintendo =[
+//     "switch",
+//     "Gameboy",
+//     "Gameboy Advanced",
+//     "Nintendo64"
+// ];
+
+// function letsplay(Games){
+//     for(var n=0;n<Games.length;n++){
+//         console.log(Games[n]);
+//     }
+// }
+// letsplay(Nintendo);
+// console.log("==========more practice=====");
+
+// var heros =[
+//     "SuperMario",
+//     "DOOM Guy",
+//     "Kirby",
+//     "Yoshi"
+// ];
+
+// function JapaneseHeros(NintendoClassic){
+//     for(var t=0;t<NintendoClassic.length;t++){
+//         console.log(NintendoClassic[t])
+//     }
+// }
+// JapaneseHeros(heros);
+// console.log("==========more practice=====");
+
+// var jobs=[
+//     "RGX",
+//     "IA",
+//     "Full StackDev",
+//     "JhonnysAuto"
+// ];
+
+// function hustle(Cashflow){
+//     for(var p=0;p<Cashflow.length;p++){
+//         console.log(Cashflow[p]);
+//     }
+// }
+// hustle(jobs);
+// console.log("==========more practice=====");
+
+// var cards=
+// [
+//     "Yu-Gi-Oh",
+//     "Pokemon",
+//     "Digimon",
+//     "UNO"
+// ];
+
+// function LetsDuel(Deck){
+//     for(var y=0;y<Deck.length;y++){
+//         console.log(Deck[y]);
+//     }
+// }
+
+// LetsDuel(cards);
+// console.log("==========more practice=====");
+
+// var cities=
+// [
+//     "NYC",
+//     "Chicago",
+//     "LA",
+//     "Charlotte",
+//     "Miami"
+// ];
+// function vacation(trip){
+//     for(var v=0;v<trip.length;v++){
+//         console.log(trip[v]);
+//     }
+// }
+// vacation(cities);
+// console.log("==========more practice=====");
+
+// var tech=[
+//     "JS",
+//     "Java",
+//     "ReactJS",
+//     "Ruby on Rails"
+// ];
+// function Learn(NewLibrarys){
+//     for (var c=0;c<NewLibrarys.length;c++){
+//         console.log(NewLibrarys[c]);
+//     }
+// }
+// Learn(tech);
+// console.log("==========more practice=====");
+
+// var Ride=[
+//     "Ferrari",
+//     "McLaren",
+//     "GTR",
+//     "Porsche",
+//     "Lambo"
+// ];
+
+// function Whip(Foreign){
+//     for (var e=0;e<Foreign.length;e++){
+//         console.log(Foreign[e])
+//     }
+// }
+// Whip(Ride);
+// console.log("==========more practice=====");
+
+// var Skills=[
+//     "Weld",
+//     "Code",
+//     "Paint",
+//     "Sneaker Resell",
+//     "Stocks",
+//     "Crypto"
+// ];
+// function Money(GetPaid){
+//     for(var u=0;u<GetPaid.length;u++){
+//         console.log("i get paid by "+ GetPaid[u])
+//     }
+// }
+// Money(Skills);
+// console.log("==========more practice=====");
+
+// var movies=[
+//     "Godzilla",
+//     "Kong",
+//     "The Matrix",
+//     "The Wolf of Wallstreet"
+// ];
+// function Tunein(Blocbusters){
+//     for (var w=0;w<Blocbusters.length;w++){
+//         console.log(Blocbusters[w]+" is the best movie ever! ");
+//     }
+// }
+// Tunein(movies);
+// console.log("==========more practice=====");
+
+// var christmas=[
+//     "Gracie",
+//     "Mom",
+//     "Dad",
+//     "Jordan"
+// ];
+
+// function Gifts(Shopping){
+//     for(var d=0;d<Shopping.length;d++){
+//         console.log(Shopping[d]+" needs a gift");
+//     }
+// }
+// Gifts(christmas);
+// console.log("==========more practice=====");
+
+// var firstpersonShooter=[
+//     "Call of Duty",
+//     "Resident Evil",
+//     "Medal Of Honor",
+//     "DOOM"
+// ];
+
+// function ShootEmUp(Glock){
+//     for(var q=0;q<Glock.lenght;q++){
+//         console.log(Glock[q] + " Lock & Load");
+//     }
+// }
+// ShootEmUp(firstpersonShooter);
+// console.log("==========more practice=====");
