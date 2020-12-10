eade7931548ad59d9ff3d8d2567847818181bc25
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-10 12:33:16 -0500

    day 3

---
+++ Javascript_practice/20-hardloop/practice/hard_loop.js
@@ -0,0 +1,71 @@
+console.log("====================PRACTICE===================")
+// var MexicanFood=[
+//     "tacos",
+//     "quesadillas",
+//     "tortas",
+//     "horchata"
+// ];
+// var ArrayForMFood = MexicanFood.length;
+// for(var i=0; i <ArrayForMFood;i++){
+//     console.log(MexicanFood[i]);
+//     if(MexicanFood[i].charAt(0) === "t" || MexicanFood[i].charAt(0) === "h"){
+//         alert("t and h were found in the back end js");
+//     }
+// }
+console.log("====================PRACTICE===================")
+
+// var videoGames=[
+//     "DOOM",
+//     "DragonBall Z",
+//     "GrandTheft Auto",
+//     "Grand Turismo",
+//     "Call Of Duty"
+// ];
+// var Gamelist= videoGames.length;
+// for(var o=0; o < Gamelist;o++){
+//     console.log(videoGames[o]);
+//         if(videoGames[o].charAt(0) === "D" || videoGames[o].charAt(0) === "G"){
+//             alert("G and D are the first letters found in the gamelist");
+//         }
+// }
+console.log("====================PRACTICE===================")
+
+var lunchTime=[
+    "pizza",
+    "steak & hoagie",
+    "starbucks",
+    "chick-Fil-A",
+    "mexican"
+];
+
+var foodlist= lunchTime.length;
+
+for (var f = 0; f < foodlist;f++){
+    console.log(lunchTime[f]);
+    if(lunchTime[f].charAt(0) === "p" || lunchTime[f].charAt(0) === "m"){
+        alert("The letter m & p were found");
+    }
+}
+console.log("====================PRACTICE===================")
+
+var skills=[
+    "code",
+    "weld",
+    "paint",
+    "flooring"
+];
+
+var skillset=skills.length;
+
+for(var d = 0; d<skillset;d++){
+    console.log("i know how to " + skills[d]);
+    if(skills[d].charAt(0) === "w" || skills[d].charAt(0) === ""){
+        alert(" w was found in the back end");
+    }
+}
+console.log("====================PRACTICE===================")
+console.log("====================PRACTICE===================")
+console.log("====================PRACTICE===================")
+console.log("====================PRACTICE===================")
+console.log("====================PRACTICE===================")
+
