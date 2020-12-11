09d337532c42de1d8bb01049b21f7a11f1c2dfa7
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-11 13:07:30 -0500

    4th day of practice

---
+++ Javascript_practice/21-EventListener/practice/javascript.js
@@ -0,0 +1,22 @@
+var InputText = document.getElementById("User-text");
+    document.onkeyup = function (event) {
+            InputText.textContent = event.key;
+    };
+
+    // =========== ^^1st container onkey
+
+// var SecondInput = document.getElementById("Second-text");
+    //     document.onkeyup = function (event) {
+    //         SecondInput.textContent = event.key;
+    //     };
+
+    // =========== ^^2nd container onkey
+
+var ThirdInput= document.getElementById("Third-text");
+    document.onkeyup = function (event) {
+        ThirdInput.textContent = event.key;
+    };
+
+// ===========^^3rd container onkey
+
+var FourthInput = document.getElementById
