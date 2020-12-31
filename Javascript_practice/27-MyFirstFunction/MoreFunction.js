fe7d515de58ad97396a39380f17365b42af542b2
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-31 13:02:51 -0500

    strings

--- Javascript_practice/27-MyFirstFunction/MoreFunction.js
+++ Javascript_practice/27-MyFirstFunction/MoreFunction.js
@@ -7,8 +7,37 @@ function adding( x,y,z)
 {
     console.log(x+y+z);
 }
-// creating a version of "adding" that returns the result
+// creating a version of "adding" that returns its result
 function ReturningAdding(x,y,z)
 {
     return x+y+z;
 }
+
+// creating a "multiplies" that takes in 3 arguments (x,y,z)
+// function should multiply and log the results
+
+function multiplier(x,y,z) 
+{
+    console.log(x*y*z);
+}
+
+// creating a "multiplier" that returns the result
+
+function ReturningMultiplier( x,y,z){
+    return x*y*z;
+}
+
+// "iString" will create 3 arguments that determine that they are strings
+// will display them in the console
+
+function isString( x,y,z) {
+    if(typeof x === "string" && typeof y === "string" && typeof z === "string") {
+        console.log("yep, they are all strings");
+    }
+
+    // if they are not strings we will note that
+    else {
+        console.log(" this is not a string");
+    }
+}
+
