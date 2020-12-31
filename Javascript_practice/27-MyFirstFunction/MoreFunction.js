95eeb433d2039cb715bd69840e76aac680a02fff
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-12-30 20:31:46 -0500

    calling math functions

---
+++ Javascript_practice/27-MyFirstFunction/MoreFunction.js
@@ -0,0 +1,14 @@
+// functions
+
+// The "adder" function will take in 3 arguments 
+// then add them together and display within the console
+
+function adding( x,y,z)
+{
+    console.log(x+y+z);
+}
+// creating a version of "adding" that returns the result
+function ReturningAdding(x,y,z)
+{
+    return x+y+z;
+}
